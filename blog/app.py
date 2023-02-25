875c301c05ed2497b8c9ec7ea74632aa434fd75f
solovyova-1996 <shatdar@yandex.ru>
2023-02-25 23:40:29 +0600

    lesson_3

--- blog/app.py
+++ blog/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, Response, request
+from flask import Flask
 from blog.user.views import user
 from blog.articles.views import article
-def crate_app()->Flask:
+from blog.auth.views import auth,login_manager
+from blog.models.database import db
+
+
+
+def crate_app() -> Flask:
     app = Flask(__name__)
+    app.config.from_object('config.DevConfig')
+    db.init_app(app)
+    login_manager.init_app(app)
     register_blueprinnts(app)
     return app
 
-def register_blueprinnts(app:Flask):
+
+def register_blueprinnts(app: Flask):
     app.register_blueprint(user)
     app.register_blueprint(article)
+    app.register_blueprint(auth)
+
+
---
+++ config.py
@@ -0,0 +1,26 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = environ.get('PROD_DATABASE_URI')
+
+
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
