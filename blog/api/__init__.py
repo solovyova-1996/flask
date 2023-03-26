33a53952767c3bfb6f816102bbf7b70183b70cfe
solovyova-1996 <shatdar@yandex.ru>
2023-03-26 18:46:11 +0600

    fix9

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -26,7 +26,7 @@ def init_api(app):
     event_plagin = EventPlugin()
     permission_plagin = PermissionPlugin(strict=False)
     api_spec_plugin = create_api_spec_plugin(app)
-    api = Api(app, plugins=[api_spec_plugin, event_plagin, permission_plagin, ])
+    api = Api(app, plugins=[api_spec_plugin, event_plagin,  ])
     api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
     api.route(ArticleList, "article_list", "/api/articles/", tag="Article")
